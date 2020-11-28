f78b9e54a6b17a90db3059ac4884915f34239e8a
elios-cama <elios.cama@gmail.com>
2020-11-28 23:19:07 +0100

    added magical background

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,64 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
+void main() => runApp(BallPage());
 
-void main() => runApp(
-      MaterialApp(
-        home: null,
+class BallPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.purple[700],
+          title: Text(
+            'ask me anything...',
+            style: TextStyle(
+              fontFamily: 'Wizard',
+              fontSize: 40,
+            ),
+          ),
+        ),
+        body: Ball(),
       ),
     );
+  }
+}
+
+class Ball extends StatefulWidget {
+  @override
+  _BallState createState() => _BallState();
+}
+
+class _BallState extends State<Ball> {
+  int ballNumber = 1;
+  void ChangeBall(){
+    setState(() {
+      ballNumber = Random().nextInt(5)+1;
+    });
+  }
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('images/background.webp'),
+            fit: BoxFit.cover,
+          ),
+        ),
+        child: Center(
+          child: FlatButton(
+          onPressed: (){
+            ChangeBall();
+          },
+          child: Image(
+            image: AssetImage('images/ball$ballNumber.png'),
+          ),
+        ),
+
+      ),),
+
+    );
+  }
+}
